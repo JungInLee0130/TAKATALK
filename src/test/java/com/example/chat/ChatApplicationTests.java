3cb02b2be26de535b6929d26c7ed16de73ad36e1
junginLee0130 <dlwjddls0130@naver.com>
2026-01-11 10:31:21 +0900

    chore: ci/cd application-test.yml 추가

--- src/test/java/com/example/chat/ChatApplicationTests.java
+++ src/test/java/com/example/chat/ChatApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")     // application-test.yml 사용
 class ChatApplicationTests {
 
     @Test
